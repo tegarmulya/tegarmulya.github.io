8927ad4aa731b8b057c6f7ae6980f618757af192
tegarmulya <tegarmulyarahman@gmail.com>
2026-06-04 19:57:21 +0700

    script.js

--- script.js
+++ script.js
@@ -587,10 +587,10 @@ scene.add(planet);
 
 //Ganti Deskripsi Planet
 const ringTexts = [
-  'Galaxy of love From ...',//untuk deskripsi planet layer 1
-  "I love you",//untuk deskripsi planet layer 2
+  'Galaxy of love From sky',//untuk deskripsi planet layer 1
+  "LOGIN ROBLOX WOI",//untuk deskripsi planet layer 2
   "♡Happy Girlfriend day♡",//untuk deskripsi planet layer 3
-  "01/08/2025",//untuk deskripsi planet layer 4
+  "04/06/2026",//untuk deskripsi planet layer 4
   ...(window.dataCCD && window.dataCCD.data.ringTexts ? window.dataCCD.data.ringTexts : [])
 ];
 
